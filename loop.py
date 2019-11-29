a882472f7bc67c4fe8b01b4731a5905235e8b6f0
cornelliusyudhawijaya <32679460+cornelliusyudhawijaya@users.noreply.github.com>
2019-11-29 11:46:51 +0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,53 @@
+# angka = 1
+# while(angka<=10):
+#     print(angka)
+#     angka +=1
+
+# # listItem = list(range(1, 11, 2))
+# # print(listItem)
+
+# # for kucing in range(1, 11, 2):
+# #     print(kucing)
+
+# z = ''
+# for item in range(5):
+#     for item1 in range(5):
+#         z += ' * '
+#     if item < 4:
+#         z += '\n'
+# print(z) 
+
+
+##Loop Statement (Statement yang cuman muncul di dalam loop (mau while ataupun for in))
+
+# print('Contoh Break')
+# for i in range(10):
+#     if i == 5:
+#         break
+#     else:
+#         print(i)    
+
+
+# print(sum([True,True, False]))
+
+
+# a = list(range(0,3))
+
+# #Menjalankan di bawahnya
+# print('Contoh pass')
+# for item in a:
+#     if not item:
+#         pass
+#     else:
+#         print(item *2)
+#     print(item)
+
+# #Tidak menjalankan dibawahnya, kembali ke awal
+# print('Contoh continue')
+# for item in a:
+#     if not item:
+#         continue
+#         for i in range(2):
+#             print('in')
+#     print(item)    
+
