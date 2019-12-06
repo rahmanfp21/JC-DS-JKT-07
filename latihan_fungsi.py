8b6de2aaa4130e702c3dac3ad84f99492df1acf2
cornelliusyudhawijaya <32679460+cornelliusyudhawijaya@users.noreply.github.com>
2019-12-06 11:38:01 +0700

    Add files via upload

---
+++ latihan_fungsi.py
@@ -0,0 +1,21 @@
+def fungsi(x):
+    return 0
+
+def fungsi2(x, y):
+    return 0
+
+def fungsi3(z):
+    return [0]
+
+def fungsi4():
+    return 0
+
+Contoh = [{'Test_1': [{0 : print('Berhasil')}]}]
+z = 0
+x = 0
+y = 1797676
+
+
+
+Contoh[fungsi(x)]['Test_1'][fungsi2(x, y)][fungsi3(z)[fungsi4()]]
+
